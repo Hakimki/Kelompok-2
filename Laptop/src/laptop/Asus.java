7a0b86e976bfa03cdf33a4f8ab0293dddd1f6370
Hakimki <97220428+Hakimki@users.noreply.github.com>
2022-11-28 08:03:51 +0700

    Update Asus.java

--- Laptop/src/laptop/Asus.java
+++ Laptop/src/laptop/Asus.java
@@ -9,6 +9,25 @@ package laptop;
  *
  * @author Fahmi Hakim
  */
-public class Asus {
+public class Asus extends Laptop {
+    
+     public static void main(String[] args) {
+        
+        Laptop hooh = new Laptop();
+        hooh.merkLaptop = "Hp Pavilion Gaming 15";
+        
+       hooh.setMerkLaptop("Lenovo Ideapad 3");
+       hooh.setJenisProsessor("AMD Ryzen 7 5000H");
+       hooh.setKodeLaptop("14ALC6");
+       hooh.setTahun(2020);
+       
+       
+        System.out.println("Merk Laptop :" + hooh.getMerkLaptop());
+        System.out.println("Kode Laptop :" + hooh.getKodeLaptop());
+        System.out.println("Prosessor :" + hooh.getJenisProsessor());
+        System.out.println("Tahun Keluar :" + hooh.getTahun());
+    }
+   
     
 }
+
